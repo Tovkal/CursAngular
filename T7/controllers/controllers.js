865c04b72974b233115776ff1bbfeb940a40e288
tovkal <Tovkal@gmail.com>
2015-12-02 18:01:06 +0100

    ng-Table

--- T7/controllers/controllers.js
+++ T7/controllers/controllers.js
@@ -1,17 +1,49 @@
-app.constant('peliculas', [
-		{name: 'Boyhood', year: 2015, oscar: 'no'},
-		{name: 'Birdman', year: 2015, oscar: 'si'},
-		{name: 'El gran hotel Budapest', year: 2015, oscar: 'no'},
-		{name: '12 años de esclavitud', year: 2014, oscar: 'si'},
-		{name: 'Gravity', year: 2014, oscar: 'no'},
-		{name: 'Her', year: 2014, oscar: 'no'},
-		{name: 'La vida de Pi', year: 2013, oscar: 'no'},
-		{name: 'Django Desencadenado', year: 2013, oscar: 'no'},
-		{name: 'Argo', year: 2013, oscar: 'si'},
-		{name: 'Lincoln', year: 2013, oscar: 'no'}
-	]);
+app.constant('peliculas', [{
+	name: 'Boyhood',
+	year: 2015,
+	oscar: 'no'
+}, {
+	name: 'Birdman',
+	year: 2015,
+	oscar: 'si'
+}, {
+	name: 'El gran hotel Budapest',
+	year: 2015,
+	oscar: 'no'
+}, {
+	name: '12 años de esclavitud',
+	year: 2014,
+	oscar: 'si'
+}, {
+	name: 'Gravity',
+	year: 2014,
+	oscar: 'no'
+}, {
+	name: 'Her',
+	year: 2014,
+	oscar: 'no'
+}, {
+	name: 'La vida de Pi',
+	year: 2013,
+	oscar: 'no'
+}, {
+	name: 'Django Desencadenado',
+	year: 2013,
+	oscar: 'no'
+}, {
+	name: 'Argo',
+	year: 2013,
+	oscar: 'si'
+}, {
+	name: 'Lincoln',
+	year: 2013,
+	oscar: 'no'
+}]);
 
-app.controller('PeliculasController', function($scope, peliculas){
+app.controller('PeliculasController', function($scope, peliculas, NgTableParams) {
 	$scope.filterByAward = false;
 	$scope.peliculas = peliculas;
+	$scope.tableParams = new NgTableParams({}, {
+		dataset: peliculas
+	});
 });
