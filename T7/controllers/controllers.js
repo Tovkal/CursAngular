009a86d41e57c81bf5576035e0fefa73df6d70a4
tovkal <Tovkal@gmail.com>
2015-12-02 16:48:41 +0100

    T7

---
+++ T7/controllers/controllers.js
@@ -0,0 +1,17 @@
+app.constant('peliculas', [
+		{name: 'Boyhood', year: 2015, oscar: 'no'},
+		{name: 'Birdman', year: 2015, oscar: 'si'},
+		{name: 'El gran hotel Budapest', year: 2015, oscar: 'no'},
+		{name: '12 años de esclavitud', year: 2014, oscar: 'si'},
+		{name: 'Gravity', year: 2014, oscar: 'no'},
+		{name: 'Her', year: 2014, oscar: 'no'},
+		{name: 'La vida de Pi', year: 2013, oscar: 'no'},
+		{name: 'Django Desencadenado', year: 2013, oscar: 'no'},
+		{name: 'Argo', year: 2013, oscar: 'si'},
+		{name: 'Lincoln', year: 2013, oscar: 'no'}
+	]);
+
+app.controller('PeliculasController', function($scope, peliculas){
+	$scope.filterByAward = false;
+	$scope.peliculas = peliculas;
+});
